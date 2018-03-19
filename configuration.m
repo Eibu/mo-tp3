e77f6a6c41f67aa5d339f0fbbafcb697c429500c
Kévin Seroux <contact@kevin-seroux.me>
2018-03-19 14:18:45 -0400

    List of problem

--- configuration.m
+++ configuration.m
@@ -15,7 +15,7 @@ classdef configuration
     end
     
     methods
-        function obj = configuration(countCities, popSize, maxGen, pm, cross, mutate)
+        function obj = configuration(problem, popSize, maxGen, pm, cross, mutate)
             obj.population_size = popSize;
             obj.max_generation = maxGen;
             obj.pm = pm;
@@ -26,8 +26,7 @@ classdef configuration
             obj.select = @(conf, pop) tournament(conf, pop);
             obj.evaluate = @(dist_matrix, pop) evaluation(dist_matrix, pop);
             obj.replace = @(conf, p, c) replace_worst(conf, p, c);
-            obj.distance_matrix = generate_distance_matrix(...
-                generate_problem(countCities));
+            obj.distance_matrix = generate_distance_matrix(problem);
         end
     end
 end
--- main.m
+++ main.m
@@ -1,13 +1,29 @@
-% Param 1: Problem size (count of cities) >= 4
+load('data.mat');
+
+% ======================================================================
+% PROBLEM LOAD METHOD (let only one commented)
+% ----------------------------------------------------------------------
+% Load an existant problem (data10, ...100, ...1000, ...10000, ...100000
+problem = data10;
+% ----------------------------------------------------------------------
+% Generate a new problem with param as count cities (must be >= 4)
+%problem = generate_problem(4);
+% ======================================================================
+
+% ============================================
+% CONFIGURATION
+% --------------------------------------------
+% Param 1: Problem
 % Param 2: Population size
 % Param 3: Count max generation
 % Param 4: Mutation probability
 % Param 5: Crossover function
 % Param 6: Mutation function
-config = configuration(4, 100, 100, 0.1, ...
+config = configuration(problem, 100, 100, 0.1, ...
     @(conf, pop) mockCrossover(conf, pop), ...
     @(conf, pop) move(conf, pop) ...
 );
+% ================================================================
 
 ga = genetic_algorithm(config);
 ga.run();
